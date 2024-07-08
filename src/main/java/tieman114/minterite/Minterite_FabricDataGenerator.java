350669c4552fc7ef44541c66cd4dde402e783173
Andrei Gubani <andrei.gubani22@gmail.com>
2024-07-08 14:53:27 +0200

    first commit

---
+++ src/main/java/tieman114/minterite/Minterite_FabricDataGenerator.java
@@ -0,0 +1,11 @@
+package tieman114.minterite;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Minterite_FabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
